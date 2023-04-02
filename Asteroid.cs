c44b2fa7070f84747f8534a8b35c4561ac6cba83
Gabriel Kanakares <gabe.kanakares@gmail.com>
2023-04-01 23:58:47 -0500

    Cleaned up code, but it crashes when you run it

--- Asteroid.cs
+++ Asteroid.cs
@@ -6,53 +6,29 @@ using static LoopingRigidBody2D;
 
 public class Asteroid : LoopingRigidBody2D
 {
-	//Keep track of asteroids we have created
-	public static List<Asteroid> asteroids {get; private set;} = new List<Asteroid>();
-	//Main will use this list to add newly created asteroids to Tree
-	public static List<Asteroid> newAsteroids {get; private set;} = new List<Asteroid>();
-	public int stage {get; private set;} 
-	//Create random numbers to intialize this asteroid.
-	RandomNumberGenerator rng = new RandomNumberGenerator();
-	
-	//int number is the number of asteroids we are adding
-	//int stage is the asteroid stage, 1 being the first and biggest stage,
-	// 3 being the smallest and final stage.
-	//When an asteroid is shot, it is destroyed and replaced by two asteroids
-	// of the proceeding stage. 
-	public static void addAsteroids(List<Vector2> positions, int stage)
+	//This is how OnBulletHitAsteroid will tell Main to create new asteroids
+	public static List<(Vector2 position, int stage)> newAsteroids 
 	{
-		foreach (Vector2 pos in positions)
-		{
-			//Use the Asteroid scene to create a new instance.
-			Asteroid ast = (Asteroid) Main.asteroidScene.Instance();
-
-			//set position and stage before adding to tree
-			ast.GlobalPosition = pos;
-			ast.stage = stage;
-			//Add to newAsteroids so Main can add to Tree.
-			newAsteroids.Add(ast);
-		}
+		//newAsteroids should be cleared when it is accessed
+		get { var ret = new List<(Vector2, int)>(newAsteroids); newAsteroids.Clear(); return ret; }
+		private set { newAsteroids = value; }
 	}
+	public int stage { set; get; }
+	private RandomNumberGenerator rng = new RandomNumberGenerator(); 
 	
+	static Asteroid()
+	{
+		newAsteroids = new List<(Vector2, int)>(); 
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//So we don't get deterministic asteroids
 		rng.Randomize();
-		//random starting velocity
 		this.LinearVelocity = 
 			new Vector2(rng.RandfRange(-100, 100), rng.RandfRange(-100, 100));
-		//random starting rotation
 		this.Rotation = rng.RandfRange(-100, 100);
-		//random angular velocity (how fast it spins)
 		this.AngularVelocity = rng.RandfRange(-1, 1);
-		asteroids.Add(this);
-	}
-
-	//make sure to remove Asteroids from list before they get destroyed
-	public override void _ExitTree()
-	{
-		Asteroid.asteroids.Remove(this);
 	}
 
 	//called when bullet and asteroid collide. Connection is created in Main.cs
@@ -66,10 +42,8 @@ public class Asteroid : LoopingRigidBody2D
 			if (this.stage < 3)
 			{
 				GD.Print(this);
-				List<Vector2> positions = new List<Vector2>();
-				positions.Add(new Vector2( ((RigidBody2D)body).GlobalPosition ));
- 				positions.Add(new Vector2( ((RigidBody2D)body).GlobalPosition ));
-				addAsteroids(positions, this.stage + 1);
+				newAsteroids.Add((new Vector2( ((RigidBody2D) body).GlobalPosition ), stage + 1));
+ 				newAsteroids.Add((new Vector2( ((RigidBody2D) body).GlobalPosition ), stage + 1));
 			}
 			//destroy asteroid we just hit
 			body.QueueFree();
--- Main.cs
+++ Main.cs
@@ -4,98 +4,97 @@ using System.Collections.Generic;
 
 public class Main : Node2D
 {
-	//When scenes are loaded from the disk, they are stored in the PackedScene
-	//class, which we use to create new instances.
-	public static PackedScene playerScene {get; private set;}
-	public static PackedScene asteroidScene {get; private set;}
-	public static PackedScene bulletScene {get; private set;}
+	/* When scenes are loaded from the disk, they are stored in the PackedScene
+	class, which we use to create new instances. */
+	private PackedScene playerScene = GD.Load<PackedScene>("res://Player.tscn");
+	private PackedScene asteroidScene = GD.Load<PackedScene>("res://Asteroid.tscn");
+	private PackedScene bulletScene = GD.Load<PackedScene>("res://Bullet.tscn");
 	
 	RandomNumberGenerator rng = new RandomNumberGenerator();
 	
-	//used to access instance of Player scene
+	//Keep track of asteroids we have created
+	private List<Asteroid> asteroids = new List<Asteroid>();
+
 	private Player player;
-	//Used to set value of bullet.LinearVelocity when bullet is first created
 	private Vector2 bulletVelocity = new Vector2(0, -200);
-	//bullets per second
-	private float fireRate = 2f;
-	//keep track of time since last bullet fired in seconds
-	private float timeSinceLastFire = 0;
-	//number of asteroids to spawn on startup
-	private int numInitAsteroids = 2;
-		
-	private static void LoadScenes()
+	private float fireRate = 2f;	//bullets per second
+	private float secondsSinceLastFire = 0;
+	private int numInitAsteroids = 2; //number of asteroids to spawn on startup
+	private int initStage = 1;
+
+	/// <summary>
+	/// Create new Asteroids and add them to newAsteroids list
+	/// </summary>
+	/// <param name="asteroids">List of tuples containing the positions and stages of asteroids to be created</param>
+	private void addAsteroids(List<(Vector2, int)> asteroids)
 	{
-		playerScene = GD.Load<PackedScene>("res://Player.tscn");
-		asteroidScene = GD.Load<PackedScene>("res://Asteroid.tscn");
-		bulletScene = GD.Load<PackedScene>("res://Bullet.tscn");
+		foreach ((Vector2 position, int stage) a in asteroids)
+		{
+			Asteroid ast = (Asteroid) asteroidScene.Instance();
+
+			ast.GlobalPosition = a.position;
+			ast.stage = a.stage;
+			//destroy player when ast hits it
+			ast.Connect("body_entered", player, "OnAsteroidHitShip");
+			//remove ast from asteroids list when it is destroyed
+			ast.Connect("child_exiting_tree", this, "OnAsteroidExitTree");
+			AddChild(ast);
+		}
 	}
 
 	private void addBullet()
 	{
 		Bullet bullet = (Bullet) bulletScene.Instance();
-		
-		//Every bullet's starting position will be at the player's current position
   		bullet.Position = player.Position;
-		
-		//Bullet velocity does NOT depend on the players velocity, but will 
-		//always be the same no matter what.
-		//Velocity vector should be rotated to match the player's rotation.
 		bullet.LinearVelocity = bulletVelocity.Rotated(player.Rotation);
 		
 		//Send signals to Bullets and Asteroids when they collide
-		foreach (Asteroid ast in Asteroid.asteroids){
+		foreach (Asteroid ast in asteroids)
+		{
 			bullet.Connect("body_entered", ast, "OnBulletHitAsteroid");
 			ast.Connect("body_entered", bullet, "OnAsteroidHitBullet");
 		}
-		//Add bullet to tree so it does stuff
 		AddChild(bullet);
 	}
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		//Load our scenes so we can create instances of them later
-		LoadScenes();
-		//Make random numbers random
 		rng.Randomize();
-		//Create our one and only instance of Player
 		player = (Player) playerScene.Instance();
-		//Add it to the tree so it does stuff.
-		this.AddChild(player);
-		//Create Array of positions for asteroids
-		List<Vector2> positions = new List<Vector2>();
+		AddChild(player);
+		//Create List of positions for asteroids
+		List<(Vector2 pos, int stage)> initAsteroids = new List<(Vector2, int)>();
 		//Create random positions for asteroids
 		for (int i = 0; i < numInitAsteroids; i++)
-			positions.Add(new Vector2(
+			initAsteroids.Add((new Vector2( 
 				rng.RandfRange(0, GetViewportRect().Size.x), 
-					rng.RandfRange(0, GetViewportRect().Size.y)));
-		//Add stage 1 asteroids using positions created
-		Asteroid.addAsteroids(positions, 1);
+					rng.RandfRange(0, GetViewportRect().Size.y)), initStage));
+		addAsteroids(initAsteroids);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
 		//Keep track of how long it's been since the last bullet as been fired
-		timeSinceLastFire += delta;
-		
-		foreach (Asteroid ast in Asteroid.newAsteroids) 
-		{
-			ast.Connect("body_entered", player, "OnAsteroidHitShip");
-			AddChild(ast);
-		}
-		Asteroid.newAsteroids.Clear();
+		secondsSinceLastFire += delta;
+		addAsteroids(Asteroid.newAsteroids);
 		
 		//Enter and Space are the shoot buttons
 		if(Input.IsActionPressed("ui_accept"))
 		{
 			//fireRate is bullets per second, so we divide 1 by fireRate to get
 			//the ammount of time we should wait between bullets.
-			if (timeSinceLastFire > 1f/fireRate)
+			if (secondsSinceLastFire > 1f/fireRate)
 			{
 				addBullet();
-				timeSinceLastFire = 0;
+				secondsSinceLastFire = 0;
 			}
 		}
+	}
+
+	private void OnAsteroidExitTree(Node asteroid)
+	{
+		asteroids.Remove((Asteroid) asteroid);
 	}
 }
